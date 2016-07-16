0ac39fe04b77816c3182c52ff8204db8a5cf913a
Роман Новоселов <novoselov_roman@mail.ru>
2016-07-16 09:00:05 +0300

    Просмотр репозиториев по Intent

--- app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileUserActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileUserActivity.java
@@ -1,12 +1,19 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -20,6 +27,7 @@ import com.softdesign.devintensive.utils.ConstantManager;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.StringTokenizer;
 
 
 public class ProfileUserActivity extends AppCompatActivity {
@@ -32,6 +40,7 @@ public class ProfileUserActivity extends AppCompatActivity {
     private TextView mProjects;
     private ListView mRepoList;
     private CollapsingToolbarLayout mCollapsingToolbar;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +54,15 @@ public class ProfileUserActivity extends AppCompatActivity {
         mRaiting = ((TextView) findViewById(R.id.main_tv_raiting));
         mUserBio = ((EditText) findViewById(R.id.et_about));
         mProfileImage = ((ImageView) findViewById(R.id.user_photo_img));
+        mCoordinatorLayout = ((CoordinatorLayout) findViewById(R.id.main_coordinator_container));
         setupToolbar();
         initProfileData();
+    }
+
+    public boolean deviceHaveIntentActivity(Intent intent) {
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activitiesList = packageManager.queryIntentActivities(intent, 0);
+        return activitiesList.size() > 0;
     }
 
     private void initProfileData() {
@@ -56,6 +72,18 @@ public class ProfileUserActivity extends AppCompatActivity {
         final RepositoryAdapter repositoryAdapter = new RepositoryAdapter(this, repo);
         mRepoList.setAdapter(repositoryAdapter);
 
+        mRepoList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final String url = "https://" + repo.get(i);
+                Intent githubIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                if (deviceHaveIntentActivity(githubIntent)) {
+                    startActivity(githubIntent);
+                } else {
+                    Snackbar.make(mCoordinatorLayout, R.string.string_warning_does_not_match_activity, Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
 
         int baseListViewHeight = getResources().getDimensionPixelSize(R.dimen.size_big_72);
         LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mRepoList.getLayoutParams();
