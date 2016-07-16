8bdc65e8359ef9f4f8650094f9d07083d98625e7
Роман Новоселов <novoselov_roman@mail.ru>
2016-07-16 03:42:12 +0300

    Небольшие правки

--- app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileUserActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileUserActivity.java
@@ -1,10 +1,12 @@
 package com.softdesign.devintensive.ui.activities;
 
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -54,6 +56,7 @@ public class ProfileUserActivity extends AppCompatActivity {
         mRepoList.setAdapter(repositoryAdapter);
 
         mUserBio.setText(userDto.getmBio());
+
         mRaiting.setText(userDto.getmRating());
         mCodeLines.setText(userDto.getmCodeLines());
         mProjects.setText(userDto.getmProjects());
